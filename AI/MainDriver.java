4c4987e58feddaa47e57872809f39e73d82e06e4
gochuicod <gochuicodarelleq@gmail.com>
2023-03-29 18:21:24 +0800

    Java Files Update v1.0

--- AI/MainDriver.java
+++ AI/MainDriver.java
@@ -1,8 +1,40 @@
-import Graph.*;
 import List.SinglyLinkedList.SinglyLinkedList;
+import Graph.*;
 
 public class MainDriver {
     public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.addFirst(2);
+        sll.display();
+        sll.addLast(77);
+        sll.display();
+        sll.addFirst(22);
+        sll.display();
+        sll.addLast(6);
+        sll.display();
+        sll.addLast(43);
+        sll.display();
+        sll.addLast(76);
+        sll.display();
+        sll.addLast(89);
+        sll.display();
+        
+        sll.deleteFirst();
+        sll.display();
+        sll.deleteItem(76);
+        sll.display();
+        sll.deleteLast();
+        sll.display();
+        sll.deleteFirst();
+        sll.display();
+
+        sll.deleteItem(6);
+        sll.display();
+        sll.deleteItem(43);
+        sll.display();
+        sll.deleteItem(77);
+        sll.display();
+
         GraphTraversal gt = new GraphTraversal();
         gt.addPlace("A");
         gt.addPlace("B");
@@ -23,37 +55,5 @@ public class MainDriver {
         gt.connect("F", "H");
 
         gt.breadthFirstSearch("A", "H");
-
-        // SinglyLinkedList sll = new SinglyLinkedList();
-        // sll.addFirst(2);
-        // sll.display();
-        // sll.addLast(77);
-        // sll.display();
-        // sll.addFirst(22);
-        // sll.display();
-        // sll.addLast(6);
-        // sll.display();
-        // sll.addLast(43);
-        // sll.display();
-        // sll.addLast(76);
-        // sll.display();
-        // sll.addLast(89);
-        // sll.display();
-        
-        // sll.deleteFirst();
-        // sll.display();
-        // sll.deleteItem(76);
-        // sll.display();
-        // sll.deleteLast();
-        // sll.display();
-        // sll.deleteFirst();
-        // sll.display();
-
-        // sll.deleteItem(6);
-        // sll.display();
-        // sll.deleteItem(43);
-        // sll.display();
-        // sll.deleteItem(77);
-        // sll.display();
     }
 }
